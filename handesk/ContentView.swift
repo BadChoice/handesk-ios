14b44fd723ff651c6e824f82629bcce75c6be3a9
Jordi Puigdellívol <jordi@gloobus.net>
2019-06-29 15:03:59 +0200

    Initial Commit

---
+++ handesk/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  handesk
+//
+//  Created by Jordi Puigdellívol on 29/06/2019.
+//  Copyright © 2019 Revo Systems. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
